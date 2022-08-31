14d6967687884947531203b5139d2488e0159995
samirwattarNTI <samirwattar89@gmail.com>
2022-08-31 14:21:12 +0200

    file shits
    idk wallah

--- getreiben/src/main.cpp
+++ getreiben/src/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 
 
 namespace NZ {
 
 	template <typename T>
-	void print(T)
+	void print(T t)
 	{
 		std::cout << t << "\n";
 	}
 
+	void WriteToFile(std::string filePath)
+	{
+		std::ofstream file(filePath);
+		std::string fileContent;
+		std::getline(std::cin, fileContent);
+		file << fileContent;
+		file.close();
+	}
 
 }
 int main()
@@ -17,4 +26,6 @@ int main()
 	NZ::print(122);
 	NZ::print("lol");
 	NZ::print('s');
+
+
 }
