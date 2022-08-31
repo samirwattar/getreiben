1156118b7ee14cd3b1d97d13c077b1900b87341f
samirwattarNTI <samirwattar89@gmail.com>
2022-08-31 14:08:38 +0200

    asdasd
    sadd

--- getreiben/src/main.cpp
+++ getreiben/src/main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
+
+
+namespace NZ {
+
+	template <typename T>
+	void print(T)
+	{
+		std::cout << t << "\n";
+	}
+
+
+}
 int main()
 {
-
+	NZ::print(122);
+	NZ::print("lol");
+	NZ::print('s');
 }
